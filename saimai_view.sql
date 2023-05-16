e14ea52a09864d345a9c7ffef2f091e9496552f1
Natchapol Laowiwatkasem <109839164+WarmNatchapol@users.noreply.github.com>
2023-05-16 20:49:16 +0700

    Create saimai_view.sql

---
+++ saimai_view.sql
@@ -0,0 +1,34 @@
+CREATE VIEW `warm-portfolio-project.weather_project.saimai_view` AS
+SELECT 
+  SPLIT(dt, " ")[0] AS date,
+  SPLIT(dt, " ")[1] AS time,
+  SPLIT(sunrise, " ")[1] AS sunrise_time,
+  SPLIT(sunset, " ")[1] AS sunset_time,
+  `temp` AS temperature,
+  feels_like,
+  pressure,
+  humidity,
+  dew_point,
+  uvi,
+  clouds,
+  visibility,
+  wind_speed,
+  wind_deg AS wind_degree,
+  main AS weather_main,
+  description AS weather_description,
+  aqi,
+  co,
+  `no`,
+  no2,
+  o3,
+  so2,
+  pm2_5,
+  pm10,
+  nh3,
+  wind_gust,
+  rain
+FROM `warm-portfolio-project.weather_project.weather_table_saimai`
+WHERE dt in (
+  SELECT MAX(dt)
+  FROM `warm-portfolio-project.weather_project.weather_table_saimai`
+);
